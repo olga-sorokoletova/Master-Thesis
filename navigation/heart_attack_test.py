9c1be6d5c74287d43bdec0720bc92bf565dd7968
olga <sorokoletova.1937430@studenti.unitoma1.it>
2022-11-17 12:45:36 +0100

    adding script for heart attack test

--- navigation/heart_attack_test.py
+++ navigation/heart_attack_test.py
@@ -28,9 +28,6 @@ def RAD(a):
 
 def quaternion(deg):
 	return tf.transformations.quaternion_from_euler(0, 0, RAD(deg))
-	
-def euler(q):
-	return tf.transformations.euler_from_quaternion(q)[2]
 
 def setOrientation(pose, theta):
 	q = quaternion(theta)
@@ -39,12 +36,13 @@ def setOrientation(pose, theta):
 	pose.orientation.z = q[2]
 	pose.orientation.w = q[3]
 	
-def allZeroedOrientations(thetas):
-	for th in thetas:
-		if (th != 0.0 and th % 360.0 != 0):
+				
+def alignedOrientations(thetas, thetas_d):
+	for th, th_d in zip(thetas, thetas_d):
+		if (th != th_d and th % 360 != th_d):
 			return False
 	return True
-				
+
 	
 def pose_str(p):
 	return "%.2f %.2f %.2f DEG" %(p[0], p[1], DEG(p[2]))
@@ -52,7 +50,7 @@ def pose_str(p):
 # main
 if __name__ == "__main__":
 
-	rospy.init_node('scenario2', disable_signals=True)
+	rospy.init_node('heart_attack_test', disable_signals=True)
 	rate = rospy.Rate(100)
 	rate.sleep()
 	
@@ -74,48 +72,48 @@ if __name__ == "__main__":
 	
 	# human1 (doctor): [5 13 0]
 	p1 = Pose()
-	p1.position.x = 5.0
-	p1.position.y = 13.0
+	p1.position.x = 14.8
+	p1.position.y = 10.11
 	p1.position.z = 0
-	th1 = 0.0
+	th1 = 30.0
 	setOrientation(p1, th1)
 	setpose_pub_1.publish(p1)
 	rate.sleep()
 	
 	# human2 (nurse): [5.5 14.5 -45]
 	p2 = Pose()
-	p2.position.x = 5.5
-	p2.position.y = 14.5
+	p2.position.x = 6.78
+	p2.position.y = 12.80
 	p2.position.z = 0
-	th2 = -45.0
+	th2 = 90.0
 	setOrientation(p2, th2)
 	setpose_pub_2.publish(p2)
 	rate.sleep()
 	
 	# human3 (doctor): [7 15 -90]
 	p3 = Pose()
-	p3.position.x = 7.0
-	p3.position.y = 15.0
+	p3.position.x = 18.77
+	p3.position.y = 15.29
 	p3.position.z = 0
-	th3 = -90.0
+	th3 = 60.0
 	setOrientation(p3, th3)
 	setpose_pub_3.publish(p3)
 	rate.sleep()
 	
 	# human4 (nurse): [8.5 14.5 -135]
 	p4 = Pose()
-	p4.position.x = 8.5
-	p4.position.y = 14.5
+	p4.position.x = 9.82
+	p4.position.y = 8.97
 	p4.position.z = 0
-	th4 = -135.0
+	th4 = 0.0
 	setOrientation(p4, th4)
 	setpose_pub_4.publish(p4)
 	rate.sleep()
 	
 	# human5 (doctor): [9 13 180]
 	p5 = Pose()
-	p5.position.x = 9.0
-	p5.position.y = 13.0
+	p5.position.x = 14.43
+	p5.position.y = 7.71
 	p5.position.z = 0
 	th5 = 180.0
 	setOrientation(p5, th5)
@@ -124,8 +122,8 @@ if __name__ == "__main__":
 	
 	# human6 (nurse): [8.5 11.5 135]
 	p6 = Pose()
-	p6.position.x = 8.5
-	p6.position.y = 11.5
+	p6.position.x = 14.21
+	p6.position.y = 5.44
 	p6.position.z = 0
 	th6 = 135.0
 	setOrientation(p6, th6)
@@ -134,18 +132,18 @@ if __name__ == "__main__":
 	
 	# human7 (doctor): [7 11 90]
 	p7 = Pose()
-	p7.position.x = 7.0
-	p7.position.y = 11.0
+	p7.position.x = 6.87
+	p7.position.y = 15.43
 	p7.position.z = 0
-	th7 = 90.0
+	th7 = 30.0
 	setOrientation(p7, th7)
 	setpose_pub_7.publish(p7)
 	rate.sleep()
 	
 	# human8 (nurse): [5.5 11.5 45]
 	p8 = Pose()
-	p8.position.x = 5.5
-	p8.position.y = 11.5
+	p8.position.x = 5.68
+	p8.position.y = 8.31
 	p8.position.z = 0
 	th8 = 45.0
 	setOrientation(p8, th8)
@@ -154,8 +152,8 @@ if __name__ == "__main__":
 	
 	# human9 (nurse by bed6): [16.83 22.09 -135]
 	p9 = Pose()
-	p9.position.x = 16.83
-	p9.position.y = 22.09
+	p9.position.x = 16.74
+	p9.position.y = 20.53
 	p9.position.z = 0
 	th9 = -135.0
 	setOrientation(p9, th9)
@@ -163,18 +161,27 @@ if __name__ == "__main__":
 	rate.sleep()
 	
 	print("Done")
-	rospy.sleep(3)
+	rospy.sleep(2)
 	
 	# IMPLEMENTATION
 	P = [p1,p2,p3,p4,p5,p6,p7,p8,p9]
 	Th = [th1,th2,th3,th4,th5,th6,th7,th8,th9]
 	Pubs = [setpose_pub_1, setpose_pub_2, setpose_pub_3, setpose_pub_4, setpose_pub_5, setpose_pub_6, setpose_pub_7, setpose_pub_8, setpose_pub_9]
 	
-	print("All people rotating until zero degrees...")
-	while not allZeroedOrientations(Th[:8]):
+	print("All people rotating until alignment...")
+	thetas_d = []
+	for i in range(len(P)-1):
+		theta_d = round(DEG(math.atan2(p9.position.y - P[i].position.y, p9.position.x - P[i].position.x)))
+		a, _ = divmod(theta_d, 5)
+		thetas_d.append(a * 5)
+	print("Desired orientations: ", thetas_d)
+	
+	signs = [-1 if abs(th) > abs(th_d) else 1 for th, th_d in zip(Th[:8], thetas_d)]
+
+	while not alignedOrientations(Th[:8], thetas_d):
 		for i in range(len(P)-1): # human9 excluded
-			if (Th[i] != 0.0 and Th[i] % 360.0 != 0):
-				Th[i] += 5.0
+			if (Th[i] != thetas_d[i] and Th[i] % 360.0 != thetas_d[i] % 360.0):
+				Th[i] += 5.0 * signs[i]
 				setOrientation(P[i], Th[i])
 				Pubs[i].publish(P[i])
 				#print("%d: %.2f" %(i+1, Th[i]))
@@ -182,12 +189,15 @@ if __name__ == "__main__":
 		rate.sleep()
 	print("Done")
 	
-	print("All people moving straight...")
-	for i in range(40):
+	print("All people moving...")
+	duration, step = 150, 70
+	for i in range(duration):
 		for i in range(len(P)-1): # human9 excluded
-			P[i].position.x += 0.1
+			P[i].position.x += (p9.position.x - P[i].position.x) / step
+			P[i].position.y += (p9.position.y - P[i].position.y) / step
 			Pubs[i].publish(P[i])
 		rate.sleep()
+		#rospy.sleep(0.1)
 	print("Done")
 	
 	
